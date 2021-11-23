f0d89bafa6e4a5f133afc05e7d2b92efe8d5fa3f
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-23 15:05:41 -0500

    zuma: Set camera extension package to Google package
    Change-Id: I519ebf8a1e2a9d06561471206c91c01b352ebd47

--- hentai_common.mk
+++ hentai_common.mk
@@ -10,5 +10,10 @@ $(call inherit-product, vendor/hentai/build/product/hentai_product.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-hentai
 
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
