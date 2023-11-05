3b1d4f1670e2b6bc32d7c1dbe2a5f7cdd4be48fb
Kazuki H <kazukih0205@gmail.com>
2023-11-05 23:56:30 +0900

    Rebrand

--- BoardConfig-statix.mk
+++ BoardConfig-statix.mk
@@ -6,12 +6,8 @@
 
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
-
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/calyx/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/statix/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
     device/google/zuma/android.hardware.security.rkp-service.citadel.xml \
     device/google/zuma/manifest_radio_ds.xml
--- statix_common.mk
+++ statix_common.mk
@@ -5,15 +5,9 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-calyx
+DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-statix
 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
-
-# Lineage Health
-include hardware/google/pixel/lineage_health/device.mk
-
-# Touch
-include hardware/google/pixel/touch/device.mk
