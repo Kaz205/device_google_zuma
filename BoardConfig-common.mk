AOSPA/android_device_google_gs101

Add initial aospa makefiles
Change-Id: I16ed0b3008a7e4aa63dec32fbfd7c196f157d3fb

BoardConfig-common.mk
@@ -419,3 +419,5 @@ BOARD_KERNEL_CMDLINE += at24.write_timeout=100
 BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 
 -include vendor/google_devices/gs101/proprietary/BoardConfigVendor.mk
+
+include device/google/gs101/BoardConfigAOSPA.mk

Kaz205/device_google_zuma

BoardConfig-common.mk
@@ -423,3 +423,4 @@ BOARD_PVMFWIMAGE_PARTITION_SIZE := 0x00100000
 
 # pick up library for cleaning digital car keys on factory reset
 -include vendor/google_devices/gs-common/proprietary/BoardConfigVendor.mk
+include device/google/zuma/BoardConfigAOSPA.mk