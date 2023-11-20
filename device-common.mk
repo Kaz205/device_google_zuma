PixelDust-Devices/android_device_google_gs201

Setup common aospa makefile
Change-Id: Ie95e80fae17f1eb6fca0cbcf9fce7e21c22dec36

device-common.mk
@@ -16,6 +16,7 @@
 
 -include vendor/google_devices/gs201/proprietary/telephony/device-vendor.mk
 include device/google/gs201/device.mk
+include device/google/gs201/device-common-aospa.mk
 include device/google/gs101/tts/voice_packs.mk
 
 # Telephony

Kaz205/device_google_zuma

device-common.mk
@@ -16,6 +16,7 @@
 
 -include vendor/google_devices/zuma/proprietary/telephony/device-vendor.mk
 include device/google/zuma/device.mk
+include device/google/zuma/device-common-aospa.mk
 
 # Telephony
 PRODUCT_COPY_FILES += \