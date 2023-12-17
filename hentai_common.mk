383a6a317b7f3e3cf602ed08c8f2f3f298c1c948
Kazuki H <kazukih0205@gmail.com>
2023-12-17 14:45:31 +0900

    Inherit hos vendor

--- hentai_common.mk
+++ hentai_common.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# HOS vendor
+$(call inherit-product, vendor/hentai/build/product/hentai_product.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-hentai
 
