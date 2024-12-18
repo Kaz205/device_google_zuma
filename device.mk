316a64c630f762dda824c651829e34e764f2dfa0
Kazuki Hashimoto <kazukih0205@gmail.com>
2024-12-19 02:22:17 +0900

    Revert "Disable secretkeeper"
    This reverts commit 53c4803abd8867b71270e7c973b2b8b664b5507c.

--- device.mk
+++ device.mk
@@ -102,8 +102,6 @@ ifeq ($(RELEASE_AVF_ENABLE_LLPVM_CHANGES),true)
 	endif
 endif
 
-SECRETKEEPER_ENABLED := false
-
 # OEM Unlock reporting
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	ro.oem_unlock_supported=1
